8fae821672550fca3292f960391ee55a5ac0d4bd
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2026-06-04 22:13:44 +0100

    MSLevelSelector added

---
+++ Assets/MarchingSquaresTool/PrototypeC/MSGenerator/MSLevelSelector.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace MarchingSquaresTool.PrototypeC.MSGenerator
+{
+    [RequireComponent(typeof(MSGenerator))]
+    public class MSLevelSelector : MonoBehaviour
+    {
+        [SerializeField] private string levelName;
+    }
+}
