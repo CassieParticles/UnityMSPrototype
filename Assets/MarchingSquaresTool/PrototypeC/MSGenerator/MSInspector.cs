6ba7bd96cc801104b4d0bad7b3036a0f3b7bda71
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2026-06-07 13:06:06 +0100

    Replaced MSInspector with new better one

--- Assets/MarchingSquaresTool/PrototypeC/MSGenerator/MSInspector.cs
+++ Assets/MarchingSquaresTool/PrototypeC/MSGenerator/MSInspector.cs
@@ -1,71 +1,94 @@
 ﻿using System;
-using System.Reflection;
 using UnityEditor;
-using UnityEngine;
 using UnityEngine.UIElements;
 
 namespace MarchingSquaresTool.PrototypeC.MSGenerator
 {
-    [CustomEditor(typeof(MSEditor))]
-    [Serializable]
+    [CustomEditor(typeof(MSLevelSelector),true)]
     public class MSInspector : Editor
     {
         private TextField _levelName;
-        private SerializedProperty _level;
+        private Button _loadButton;
+        private Button _saveButton;
 
-        private void PreReload()
-        {
-            _level.stringValue = _levelName.text;
-            serializedObject.ApplyModifiedProperties();
-        }
-
-        private void PostReload()
-        {
-            _levelName.value = _level.stringValue;
-        }
+        private SerializedProperty _levelNameSer;
 
         private void OnEnable()
         {
-            _level = serializedObject.FindProperty("levelName");
-            _levelName = new TextField("Level Name") {value = _level.stringValue};
+            //Get serialized field in level editor, set text box
+            _levelNameSer = serializedObject.FindProperty("levelName");
             
-            AssemblyReloadEvents.beforeAssemblyReload += PreReload;
-            AssemblyReloadEvents.afterAssemblyReload += PostReload;
+            _levelName = new TextField("Level Name") {value = _levelNameSer.stringValue};
+            _loadButton = new Button(LoadLevel) { name = "Load Level" };
+            _saveButton = new Button(SaveLevel) { name = "Save Level" };
+            
+            //Ensure name is retained in text box
+            AssemblyReloadEvents.beforeAssemblyReload += UpdateName;
+            AssemblyReloadEvents.afterAssemblyReload += UpdateTextBox;
 
-            AssemblyReloadEvents.beforeAssemblyReload += SaveLevel;
+            if (target is MSLevelEditor)
+            {
+                AssemblyReloadEvents.beforeAssemblyReload += SaveLevel;
+            }
+            AssemblyReloadEvents.afterAssemblyReload += LoadLevel;
         }
 
         private void OnDisable()
         {
-            _level.stringValue = _levelName.text;
-            serializedObject.ApplyModifiedProperties();
+            //Update serialized field in level editor with string in textbox
+            UpdateName();
             
-            AssemblyReloadEvents.beforeAssemblyReload -= PreReload;
-            AssemblyReloadEvents.afterAssemblyReload -= PostReload;
+            //Ensure name is retained in text box
+            AssemblyReloadEvents.beforeAssemblyReload -= UpdateName;
+            AssemblyReloadEvents.afterAssemblyReload -= UpdateTextBox;
             
-            AssemblyReloadEvents.beforeAssemblyReload -= SaveLevel;
+            if (target is MSLevelEditor)
+            {
+                AssemblyReloadEvents.beforeAssemblyReload -= SaveLevel;
+            }
+            AssemblyReloadEvents.afterAssemblyReload -= LoadLevel;
         }
 
-        public void LoadLevel()
+        public override VisualElement CreateInspectorGUI()
         {
-            ((MSEditor)target).LoadLevel();
+            VisualElement inspector = new  VisualElement();
+
+            inspector.Add(_levelName);
+            inspector.Add(_loadButton);
+            
+            //Only level editors support saving
+            if (target is MSLevelEditor)
+            {
+                inspector.Add(_saveButton);
+            }
+            
+            return inspector;
+        }
+
+        private void UpdateTextBox()
+        {
+            _levelName.value = _levelNameSer.stringValue;
+        }
+
+        private void UpdateName()
+        {
+            _levelNameSer.stringValue = _levelName.text;
+            serializedObject.ApplyModifiedProperties();
+        }
+
+        private void LoadLevel()
+        {
+
+            UpdateName();
+            
+            ((MSLevelSelector)target).LoadLevel();
         }
 
         private void SaveLevel()
         {
-            ((MSEditor)target).SaveLevel();
-        }
-        
-        public override VisualElement CreateInspectorGUI()
-        {
-            VisualElement inspector = new VisualElement();
+            UpdateName();
             
-            inspector.Add(_levelName);
-            
-            inspector.Add(new Button(LoadLevel) {text = "Load Level"});
-            inspector.Add(new Button(SaveLevel) {text = "Save Level"});
-            
-            return inspector;
+            ((MSLevelEditor)target).SaveLevel();
         }
     }
 }
