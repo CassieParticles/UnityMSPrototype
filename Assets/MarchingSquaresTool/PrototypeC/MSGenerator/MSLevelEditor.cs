78ffacbbb41cfab381d198fab412becd2b4bb4a3
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2026-06-07 13:05:11 +0100

    Added new level editor and level selector classes

---
+++ Assets/MarchingSquaresTool/PrototypeC/MSGenerator/MSLevelEditor.cs
@@ -0,0 +1,12 @@
+﻿using MarchingSquaresTool.PrototypeC.FileLoader;
+
+namespace MarchingSquaresTool.PrototypeC.MSGenerator
+{
+    public class MSLevelEditor: MSLevelSelector
+    {
+        public void SaveLevel()
+        {
+            LevelFileHandler.StoreLevel(Generator.Grid,levelName);
+        }
+    }
+}
--- Assets/MarchingSquaresTool/PrototypeC/MSGenerator/MSLevelSelector.cs
+++ Assets/MarchingSquaresTool/PrototypeC/MSGenerator/MSLevelSelector.cs
@@ -1,10 +1,32 @@
-﻿using UnityEngine;
+﻿using System;
+using MarchingSquaresTool.PrototypeC.FileLoader;
+using UnityEngine;
 
 namespace MarchingSquaresTool.PrototypeC.MSGenerator
 {
     [RequireComponent(typeof(MSGenerator))]
     public class MSLevelSelector : MonoBehaviour
     {
-        [SerializeField] private string levelName;
+        [SerializeField] protected string levelName;
+
+        //Safer way of getting generator
+        public MSGenerator Generator
+        {
+            get
+            {
+                if (_generator)
+                {
+                    _generator = GetComponent<MSGenerator>();
+                }
+                return _generator;
+            }
+        }
+
+        private MSGenerator _generator;
+
+        public void LoadLevel()
+        {
+            Generator.Grid = LevelFileHandler.LoadLevel(levelName);
+        }
     }
 }
