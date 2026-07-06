6494002e48d52d21f0b6b26895f49f4f23845bd8
Cassie Parsons <Cassie.Parsons2003@outlook.com>
2026-07-06 23:46:51 +0100

    Added basic player

---
+++ Assets/GameObjects/Player/PlayerActor.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Assertions.Must;
+
+namespace GameObjects.Player
+{
+    [RequireComponent(typeof(Rigidbody2D),typeof(CapsuleCollider2D))]
+    public class PlayerActor : MonoBehaviour
+    {
+        Rigidbody2D _rb;
+        CapsuleCollider2D _capsuleCollider;
+
+        [SerializeField] private float maxMoveSpeed;
+
+        [SerializeField] private float jumpForce = 500;
+
+        private Vector2 floorNormal;
+
+        private void Awake()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+            _capsuleCollider = GetComponent<CapsuleCollider2D>();
+            _rb.freezeRotation = true;
+        }
+
+        private void OnCollisionStay2D(Collision2D other)
+        {
+            List<ContactPoint2D> contacts = new List<ContactPoint2D>();
+            other.GetContacts(contacts);
+
+            foreach (var contact in contacts)
+            {
+                floorNormal = (Vector2)transform.position - contact.point;
+            }
+        }
+
+        private void OnCollisionExit2D(Collision2D other)
+        {
+            floorNormal = Vector2.zero;
+        }
+
+        //-ve for left force
+        public void ApplyMoveForce(float force)
+        {
+            //0 -> 1
+            float maxSpeedScalar = Mathf.Abs(_rb.linearVelocityX) /  maxMoveSpeed;
+            //1 -> 0
+            maxSpeedScalar = 1 - maxSpeedScalar;
+
+            if (Mathf.Sign(force) != Mathf.Sign(maxSpeedScalar) && maxSpeedScalar > 0.7)
+            {
+                //1 -> 2
+                maxSpeedScalar = 2 - maxSpeedScalar;
+            }
+            
+            Vector2 rightDirection = new Vector2(floorNormal.y, -floorNormal.x);
+            
+            _rb.AddForce(force * maxSpeedScalar * rightDirection );
+        }
+
+        public void ApplyJump()
+        {
+            _rb.AddForce(floorNormal * jumpForce);
+        }
+    }
+}
---
+++ Assets/GameObjects/Player/PlayerController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using GameObjects.WeaponSystem.Bullet;
+using GameObjects.WeaponSystem.WeaponState;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+namespace GameObjects.Player
+{
+    [RequireComponent(typeof(PlayerActor),typeof(PlayerInput))]
+    public class PlayerController : MonoBehaviour
+    {
+        private PlayerInput _playerInput;
+        private PlayerActor _playerActor;
+
+        [SerializeField] private Bullet bulletPrefab;
+        
+        private float _moveDirection = 0.0f;
+        
+        private Vector2 cursorPosition = Vector2.zero;
+
+        private void Awake()
+        {
+            _playerInput = GetComponent<PlayerInput>();
+            _playerActor = GetComponent<PlayerActor>();
+        }
+
+        private void OnEnable()
+        {
+            _playerInput.actions.FindAction("Move").performed += PlayerMove;
+            _playerInput.actions.FindAction("Move").canceled += PlayerMove;
+            
+            _playerInput.actions.FindAction("Jump").performed += PlayerJump;
+            
+            _playerInput.actions.FindAction("Aim").performed += PlayerAim;
+            
+            _playerInput.actions.FindAction("Fire").performed += PlayerFire;
+        }
+
+        private void OnDisable()
+        {
+            _playerInput.actions.FindAction("Move").performed -= PlayerMove;
+            _playerInput.actions.FindAction("Move").canceled -= PlayerMove;
+            
+            _playerInput.actions.FindAction("Jump").performed -= PlayerJump;
+            
+            _playerInput.actions.FindAction("Aim").performed -= PlayerAim;
+            
+            _playerInput.actions.FindAction("Fire").performed -= PlayerFire;
+        }
+
+        private void PlayerMove(InputAction.CallbackContext obj)
+        {
+            _moveDirection  = obj.action.ReadValue<float>();
+        }
+
+        private void PlayerJump(InputAction.CallbackContext obj)
+        {
+            _playerActor.ApplyJump();
+        }
+
+        private void PlayerAim(InputAction.CallbackContext obj)
+        {
+            cursorPosition = Camera.main.ScreenToWorldPoint(obj.ReadValue<Vector2>());
+        }
+
+        private void PlayerFire(InputAction.CallbackContext obj)
+        {
+            Vector2 direction = (cursorPosition - (Vector2)transform.position).normalized;
+            WeaponState state = new WeaponState();
+            state.bulletSize = 0.1f;
+            state.bulletSpeed = 20;
+            state.bulletDamage = 20;
+            state.explosionRadius = 5;
+            
+            Bullet bullet = Instantiate(bulletPrefab);
+            bullet.ReadyBullet((Vector2)transform.position + direction, direction,state);
+        }
+
+        private void FixedUpdate()
+        {
+            _playerActor.ApplyMoveForce(_moveDirection * 15.0f);
+        }
+    }
+}
